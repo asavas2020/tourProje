39038c114d8e85ed0cbb2fc8fb10fa6ed2e2cf1d
Gokhan <gknsvsdata@gmail.com>
2022-06-28 16:40:13 +0300

    lastcommit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,17 @@ function App() {
     return <main><Loading/></main>
   }
 
-  
+  if(tours.length === 0){
+    return(
+      <main>
+
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button className='btn' onClick={()=> fetchTours()}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
 
   return <main><Tours tours = {tours} removeTour = {removeTour}/></main>
 
